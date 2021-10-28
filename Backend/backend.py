8775fe81b8bdcc299d55be25b6f099f80e54f4d1
Kenny-Miller <kennymilarhol@gmail.com>
2021-10-27 22:55:54 -0600

    Added cors to backend

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,13 +1,30 @@
-from flask import Flask, jsonify
+import json
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello_world():
+@app.route("/getApps", methods=['Get'])
+def get_apps():
 
-  # Todo: Implement backend
+  apps = [
+    "Sage2",
+    "Panoptic"
+  ]
+  
+  response = app.response_class(
+    response=json.dumps(apps),
+    status=200,
+    mimetype='application/json'
+  )
+  return response
 
-  obj = {
-    "key": "val"
-  }
-  return obj
+@app.route("/launchApp", methods=['Post'])
+def start_app():
+  data = request.get_json()
+  print(data)
+
+  #Todo start app on each machine
+
+  return "", 200
