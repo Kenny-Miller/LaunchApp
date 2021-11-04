af62dc03c1c76d5d9ff7e30af8c3d533238b40bb
Kenny-Miller <kennymilarhol@gmail.com>
2021-11-04 13:38:20 -0600

    fleshed out sending and receiving messages between sockets

--- Sockets/client.py
+++ Sockets/client.py
@@ -1,8 +1,78 @@
-import socket
+import socket, sys
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+SERVER = '127.0.0.1'  # The server's hostname or IP address
+PORT = 5050        # The port used by the server
+HEADER = 64
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+# Determine which mode to run in
+# User mode is a one-way connection that allows you to send commands to the server
+# Auto mode is a two-way connection that waits for messages from server
+def get_mode():
+  mode = 'auto'
+  if len(sys.argv) > 1 and sys.argv[1].lower() in ['user','u']:
+    mode = 'user'
+  return mode  
+
+# Starts up the client 
+def start_client(mode):
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
+    
+    # Handles recieving messages from the server
+    # Server will send 2 messages: one with message length and one with the actual message
+    def recieve_message():
+      msg = ''
+      msg_length = conn.recv(HEADER).decode('utf-8')
+      if msg_length:
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode('utf-8')
+        print(f'[MESSAGE] client recieved {msg} from server')
+      return msg
+
+    # Handles what to do with a message sent from the server
+    def handle_message(msg):
+      pass
+
+    # Sends a message to the server
+    # Will always send two messages to the server: one stating the message length and one containg the message
+    def send_message(msg):
+      message = msg.encode('utf-8')
+      msg_length = len(message)
+      send_length = str(msg_length).encode('utf-8')
+      send_length += b' ' * (HEADER - len(send_length))
+      print(f'[MESSAGE] sending message {message} to server')
+      conn.send(send_length)
+      conn.send(message)
+    
+    # Sends a message to the server saying that the client is closing the connection
+    def close_connection():
+      send_message("!QUIT!")
+      conn.close()
+
+    conn.connect((SERVER, PORT))
+    print(f'[Connection] client connected to {SERVER}:{PORT}')
+    try:
+      running = True
+      while running:
+        if mode == 'user':
+          msg = input("[USER] Enter command: ")
+          if msg.lower() in ['quit', 'q', 'exit']:
+            running = False
+          else :
+            send_message(msg)
+        else:
+          msg = recieve_message()
+          handle_message(msg) 
+
+    except Exception as e:
+      print('An exception occurred: {}'.format(e))
+    except KeyboardInterrupt:
+      pass
+    print('[CONNECTION] Terminating connection bewteen client and server')
+    close_connection()
+
+mode = get_mode()
+start_client(mode) 
+
+      
+    
+    
--- Sockets/server.py
+++ Sockets/server.py
@@ -1,17 +1,70 @@
-import socket
+import socket, threading
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = '127.0.0.1'  
+PORT = 5050        
+HEADER = 64
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            # conn.sendall(data)
-            print(data)
+def handle_conn(conn, addr):
+  print(f'[CONNECTION] server accepted connected with {addr}')
+  connected = True
+
+  # Handles recieving messages from a client
+  # Client will send 2 messages: one with message length and one with the actual message
+  def recieve_message():
+    msg = ''
+    msg_length = conn.recv(HEADER).decode('utf-8')
+    if msg_length:
+      msg_length = int(msg_length)
+      msg = conn.recv(msg_length).decode('utf-8')
+      print(f'[MESSAGE] server recieved {msg} from {addr}')
+    return msg
+  
+  # Sends a message to a client
+  # Will always send two messages to the client: one stating the message length and one containg the message
+  def send_message(msg):
+    message = msg.encode('utf-8')
+    msg_length = len(message)
+    send_length = str(msg_length).encode('utf-8')
+    send_length += b' ' * (HEADER - len(send_length))
+    print(f'[MESSAGE] sending message {message} to client')
+    conn.send(send_length)
+    conn.send(message)
+
+  # Handles what to do with a message sent from a client
+  def handle_message(msg):
+    if msg == 'test':
+      print('made it')
+
+  # Wait and handle sending and recieveing messages to the client
+  while connected:
+      msg = recieve_message()
+      if msg == '!QUIT!':
+        connected = False
+      else:
+        handle_message(msg)
+
+  print(f'[CONNECTION] server is disconnecting from {addr}')
+  print(f'[INFO] current number of connections is {threading.active_count() -1}')
+  conn.close()
+
+def start_server():
+  # Create listening socket on server
+  print('[STARTUP] starting server')
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listener: 
+    listener.bind((HOST, PORT))
+    listener.listen(5)
+    print(f'[STARTUP] server is now listening on: {HOST}:{PORT}')
+
+    # Loop forever
+    try:
+      while True:
+        # Create a new thread that handles a connection to the server
+        (conn, addr) = listener.accept()
+        thread = threading.Thread(target=handle_conn, args=(conn,addr))
+        thread.start()
+        print(f'[INFO] current number of connections is {threading.active_count() -1}')
+    except KeyboardInterrupt:
+      print('interupt')
+      return 
+
+start_server()
